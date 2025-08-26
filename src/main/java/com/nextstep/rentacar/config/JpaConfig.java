7c9748a83ffedb190ff3532457b9d793dce0bd10
rdjakovic <zoranm26@gmail.com>
2025-08-26 23:30:05 +0200

    Warp file. Fix config.

--- src/main/java/com/nextstep/rentacar/config/JpaConfig.java
+++ src/main/java/com/nextstep/rentacar/config/JpaConfig.java
@@ -1,8 +1,14 @@
 package com.nextstep.rentacar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
 
 /**
  * JPA configuration class.
@@ -11,4 +17,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 @EnableJpaRepositories(basePackages = "com.nextstep.rentacar.repository")
 public class JpaConfig {
+
+    /**
+     * Provides consistent UTC timestamps for JPA auditing.
+     */
+    @Bean
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now(ZoneOffset.UTC));
+    }
 }
